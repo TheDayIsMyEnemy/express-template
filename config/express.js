b5570c32b029335609e3bae23c7531e27d32d905
TheDayIsMyEnemy <sickskills91@gmail.com>
2018-06-10 18:48:07 +0300

    fix

--- config/express.js
+++ config/express.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
-const multer = require('multer');
 
 module.exports = (app, config) => {
     app.engine('hbs', handlebars({
@@ -14,7 +13,5 @@ module.exports = (app, config) => {
 
     app.use(bodyParser.urlencoded({ extended: true })); 
 
-    app.use('/public', express.static(path.join(__dirname, '../public')));
-   // app.use('/public', express.static(path.normalize(path.join(config.rootPath, 'public'))))
-    
+    app.use('/public', express.static(path.join(__dirname, '../public'))); 
 }   
--- config/routes.js
+++ config/routes.js
@@ -2,20 +2,9 @@ const handlers = require('../handlers');
 const multer = require('multer');
 
 let upload = multer({
-    dest: './public/memes'
+    dest: './public/uploads'
 });
 
 module.exports = (app) => {   
     app.get('/', handlers.home.index);
-
-    app.get('/meme/add', handlers.meme.addGet);
-    app.post('/meme/add', upload.single('image'), handlers.meme.addPost);
-    app.get('/meme/all', handlers.meme.viewAll);
-    app.get('/meme/details/:id', handlers.meme.details);
-
-    app.get('/genre/add', handlers.genre.addGet);
-    app.post('/genre/add', handlers.genre.addPost);
-
-    app.use('/api', handlers.api);
-
 }
